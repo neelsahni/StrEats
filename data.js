cfe2004d486b97e19dab35fb21ba00584f6609f0
Shane Yokota <shaneyok@usc.edu>
2023-02-04 14:19:54 -0800

    json and icon

---
+++ data.js
@@ -0,0 +1,111 @@
+const geojson = {
+    "type":"FeatureCollection",
+    "features":[
+       {
+          "type":"Feature",
+          "geometry":{
+             "type":"Point",
+             "coordinates":[
+                -77.032,
+                38.913
+             ]
+          },
+          "properties":{
+             "title":"Mapbox",
+             "description":"Washington, D.C."
+          },
+          "category":"food"
+       },
+       {
+          "type":"Feature",
+          "geometry":{
+             "type":"Point",
+             "coordinates":[
+                -122.414,
+                37.776
+             ]
+          },
+          "properties":{
+             "title":"Mapbox",
+             "description":"San Francisco, California"
+          },
+          "category":"food"
+       },
+       {
+          "type":"Feature",
+          "geometry":{
+             "coordinates":[
+                -118.2800437480451,
+                34.0220331857726
+             ],
+             "type":"Point"
+          },
+          "properties":{
+             "name":"Stone Ring",
+             "description":"all the rings are handmade. I can also make a certain ring when asked. Come find artistic jewelries!"
+          },
+          "imgs":['/-kj[   q', '', ''],
+          "category":"jewelry"
+       },
+       {
+          "type":"Feature",
+          "geometry":{
+             "coordinates":[
+                -118.28099911076842,
+                34.020470085903796
+             ],
+             "type":"Point"
+          },
+          "properties":{
+             "name":"Taco Stop",
+             "description":"We got great variety of tacos from beef, pork, and chiicken., and tofu. Try out our freshly made Tacos!"
+          },
+          "category":"taco"
+       },
+       {
+          "type":"Feature",
+          "geometry":{
+             "coordinates":[
+                -118.2807520257464,
+                34.02036094487106
+             ],
+             "type":"Point"
+          },
+          "properties":{
+             "name":"Cold-break",
+             "description":"we got some ice creams for you to grab while waiting to get in line for the basketball game. $2 a piece!"
+          },
+          "category":"icecream"
+       },
+       {
+          "type":"Feature",
+          "geometry":{
+             "coordinates":[
+                -118.28232030095322,
+                34.018111734035315
+             ],
+             "type":"Point"
+          },
+          "properties":{
+             "name":"Doughnuts",
+             "description":"We made a donum with a coockie dough inside. Try out our new kinds of donuts!"
+          },
+          "category":"donut"
+       },
+       {
+          "type":"Feature",
+          "geometry":{
+             "coordinates":[
+                -118.29161228355197,
+                34.02555325099823
+             ],
+             "type":"Point"
+          },
+          "properties":{
+             "name":"J's Grill and Kebab",
+             "description":"We sell Kebab and many other grilled items such as hamberger. Come get some food!"
+          },
+          "category":"Kebab"
+       }
+    ]
+ }
